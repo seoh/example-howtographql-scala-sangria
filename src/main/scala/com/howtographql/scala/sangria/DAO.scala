d816faada18fb3fd3cb2bc883a27f8ba953e626c
seoh <devthewild@gmail.com>
2020-06-18 02:38:09 +0900

    2/ Define GraphQLSchema

--- src/main/scala/com/howtographql/scala/sangria/DAO.scala
+++ src/main/scala/com/howtographql/scala/sangria/DAO.scala
@@ -1,6 +1,7 @@
 package com.howtographql.scala.sangria
 import slick.jdbc.H2Profile.api._
+import DBSchema._
 
 class DAO(db: Database) {
-
+  def allLinks = db.run(Links.result)
 }
--- src/main/scala/com/howtographql/scala/sangria/GraphQLSchema.scala
+++ src/main/scala/com/howtographql/scala/sangria/GraphQLSchema.scala
@@ -1,7 +1,31 @@
 package com.howtographql.scala.sangria
 
-import sangria.schema.Schema
+import sangria.schema.ObjectType
+import sangria.schema.Field
+import sangria.schema.ListType
+
+import sangria.schema.{IntType, StringType, Schema, fields}
+import sangria.macros.derive._
+import models.Link
+
 
 object GraphQLSchema {
-  val SchemaDefinition = ???
+
+  val LinkType = ObjectType[Unit, Link](
+    "Link",
+    fields[Unit, Link](
+      Field("id", IntType, resolve = _.value.id),
+      Field("url", StringType, resolve = _.value.url),
+      Field("description", StringType, resolve = _.value.description)
+    )
+  )
+
+  val QueryType = ObjectType(
+    "Query",
+    fields[MyContext, Unit](
+      Field("allLinks", ListType(LinkType), resolve = c => c.ctx.dao.allLinks)
+    )
+  )
+
+  val SchemaDefinition = Schema(QueryType)
 }

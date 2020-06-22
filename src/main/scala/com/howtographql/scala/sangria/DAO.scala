9df40d29904e77011a0b68b217d92f92371f9eca
seoh <devthewild@gmail.com>
2020-06-22 21:39:06 +0900

    4/ Test it

--- src/main/scala/com/howtographql/scala/sangria/DAO.scala
+++ src/main/scala/com/howtographql/scala/sangria/DAO.scala
@@ -8,6 +8,8 @@ import com.howtographql.scala.sangria.models.Link
 class DAO(db: Database) {
   def allLinks = db.run(Links.result)
 
-  def getLinks(ids: Seq[Int]): Future[Seq[Link]] =
+  def getLinks(ids: Seq[Int]): Future[Seq[Link]] = {
+    println(s"DAO.getLinks [${ids.mkString(", ")}]")
     db.run(Links.filter(_.id inSet ids).result)
+  }
 }

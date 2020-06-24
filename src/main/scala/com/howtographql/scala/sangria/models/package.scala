883b54b7bf956b4391e4b50734b29346ab89e47f
seoh <devthewild@gmail.com>
2020-06-24 19:34:27 +0900

    5/ Extend a Link model
    It will not be compiled. `DateTime` doen not have two mapper. One is for
    Slick, need a mapper for storing as `Timestamp`. Another is for GraphQL,
    how to be described in Query and represented in JSON. Two mapper will be
    written in next two parts.

--- src/main/scala/com/howtographql/scala/sangria/models/package.scala
+++ src/main/scala/com/howtographql/scala/sangria/models/package.scala
@@ -1,5 +1,7 @@
 package com.howtographql.scala.sangria
 
+import akka.http.scaladsl.model.DateTime
+
 package object models {
-  case class Link(id: Int, url: String, description: String)
+  case class Link(id: Int, url: String, description: String, createdAt: DateTime)
 }

24c57297129213b349b3f857202515aa2c17ebe4
seoh <devthewild@gmail.com>
2020-06-24 20:51:32 +0900

    5/ Fix the database
    Add a column and column mapper to Slick. As said prev commit, it will
    not be compiled before another mapper is provided.

--- src/main/scala/com/howtographql/scala/sangria/DBSchema.scala
+++ src/main/scala/com/howtographql/scala/sangria/DBSchema.scala
@@ -7,17 +7,24 @@ import scala.concurrent.Await
 import scala.language.postfixOps
 
 import com.howtographql.scala.sangria.models._
+import akka.http.scaladsl.model.DateTime
+import java.sql.Timestamp
 
 object DBSchema {
+
+  implicit val dateTimeColumnType = MappedColumnType.base[DateTime, Timestamp](
+    dt => new Timestamp(dt.clicks),
+    ts => DateTime(ts.getTime)
+  )
 
   class LinksTable(tag: Tag) extends Table[Link](tag, "LINKS"){
 
     def id = column[Int]("ID", O.PrimaryKey, O.AutoInc)
     def url = column[String]("URL")
     def description = column[String]("DESCRIPTION")
+    def createdAt = column[DateTime]("CREATED_AT")
 
-    def * = (id, url, description).mapTo[Link]
-
+    def * = (id, url, description, createdAt).mapTo[Link]
   }
 
   val Links = TableQuery[LinksTable]
@@ -26,9 +33,9 @@ object DBSchema {
     Links.schema.create,
 
     Links forceInsertAll Seq(
-      Link(1, "http://howtographql.com", "Awesome community driven GraphQL tutorial"),
-      Link(2, "http://graphql.org", "Official GraphQL web page"),
-      Link(3, "https://facebook.github.io/graphql/", "GraphQL specification")
+      Link(1, "http://howtographql.com", "Awesome community driven GraphQL tutorial", DateTime(2017,9,12)),
+      Link(2, "http://graphql.org", "Official GraphQL web page", DateTime(2017, 10, 1)),
+      Link(3, "https://facebook.github.io/graphql/", "GraphQL specification", DateTime(2017, 10, 2))
     )
   )
 

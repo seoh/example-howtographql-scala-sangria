0ef973559bc1cce23119b04a2091bde2b0cb4f2c
seoh <devthewild@gmail.com>
2020-06-25 04:57:30 +0900

    7/ Preparing database
    Add `postedBy` column to Link table and set it as foreign key of Id in
    User table. `userId` and `linkId` of Vote table is set as foreign key of
    Id in User table and Id in Link table. Aranged to define tables and run
    queries for tables' heirarchy. User should be before on Link and Vote,
    Link should be before on Vote.

--- src/main/scala/com/howtographql/scala/sangria/DBSchema.scala
+++ src/main/scala/com/howtographql/scala/sangria/DBSchema.scala
@@ -17,19 +17,6 @@ object DBSchema {
     ts => DateTime(ts.getTime)
   )
 
-  class LinksTable(tag: Tag) extends Table[Link](tag, "LINKS"){
-
-    def id = column[Int]("ID", O.PrimaryKey, O.AutoInc)
-    def url = column[String]("URL")
-    def description = column[String]("DESCRIPTION")
-    def createdAt = column[DateTime]("CREATED_AT")
-
-    def * = (id, url, description, createdAt).mapTo[Link]
-  }
-
-  val Links = TableQuery[LinksTable]
-
-
   class UsersTable(tag: Tag) extends Table[User](tag, "USERS"){
     def id = column[Int]("ID", O.PrimaryKey, O.AutoInc)
     def name = column[String]("NAME")
@@ -43,11 +30,31 @@ object DBSchema {
   val Users = TableQuery[UsersTable]
 
 
+
+  class LinksTable(tag: Tag) extends Table[Link](tag, "LINKS"){
+
+    def id = column[Int]("ID", O.PrimaryKey, O.AutoInc)
+    def url = column[String]("URL")
+    def description = column[String]("DESCRIPTION")
+    def postedBy = column[Int]("USER_ID")
+    def createdAt = column[DateTime]("CREATED_AT")
+
+    def postedByFK = foreignKey("postedBy_FK", postedBy, Users)(_.id)
+
+    def * = (id, url, description, postedBy, createdAt).mapTo[Link]
+  }
+
+  val Links = TableQuery[LinksTable]
+
+
   class VotesTable(tag: Tag) extends Table[Vote](tag, "VOTES"){
     def id = column[Int]("ID", O.PrimaryKey, O.AutoInc)
     def userId = column[Int]("USER_ID")
     def linkId = column[Int]("LINK_ID")
     def createdAt = column[DateTime]("CREATED_AT")
+
+    def userFK = foreignKey("user_FK", userId, Users)(_.id)
+    def linkFK = foreignKey("link_FK", linkId, Links)(_.id)
 
     def * = (id, userId, linkId, createdAt).mapTo[Vote]
   }
@@ -56,19 +63,19 @@ object DBSchema {
 
 
   val databaseSetup = DBIO.seq(
-    Links.schema.create,
     Users.schema.create,
+    Links.schema.create,
     Votes.schema.create,
-
-    Links forceInsertAll Seq(
-      Link(1, "http://howtographql.com", "Awesome community driven GraphQL tutorial", DateTime(2017,9,12)),
-      Link(2, "http://graphql.org", "Official GraphQL web page", DateTime(2017, 10, 1)),
-      Link(3, "https://facebook.github.io/graphql/", "GraphQL specification", DateTime(2017, 10, 2))
-    ),
 
     Users forceInsertAll Seq(
       User(1, "mario", "mario@example.com", "s3cr3t"),
       User(2, "Fred", "fred@flinstones.com", "wilmalove")
+    ),
+
+    Links forceInsertAll Seq(
+      Link(1, "http://howtographql.com", "Awesome community driven GraphQL tutorial", 1, DateTime(2017,9,12)),
+      Link(2, "http://graphql.org", "Official GraphQL web page", 1, DateTime(2017, 10, 1)),
+      Link(3, "https://facebook.github.io/graphql/", "GraphQL specification", 2, DateTime(2017, 10, 2))
     ),
 
     Votes forceInsertAll Seq(

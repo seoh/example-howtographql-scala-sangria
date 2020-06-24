99522fd8d055533ad92c9134c49727673d5d6bf0
seoh <devthewild@gmail.com>
2020-06-24 21:38:40 +0900

    5/ Define custom scalar for DateTime

--- src/main/scala/com/howtographql/scala/sangria/GraphQLSchema.scala
+++ src/main/scala/com/howtographql/scala/sangria/GraphQLSchema.scala
@@ -6,18 +6,45 @@ import sangria.schema.ListType
 
 import sangria.schema.{IntType, StringType, Schema, fields}
 import sangria.macros.derive._
-import models.Link
+import models.{Link, DateTimeCoerceViolation}
 import sangria.schema.OptionType
 import sangria.schema.Argument
 import sangria.schema.ListInputType
 import sangria.execution.deferred.Fetcher
 import sangria.execution.deferred.HasId
 import sangria.execution.deferred.DeferredResolver
+import sangria.schema.ScalarType
+import akka.http.scaladsl.model.DateTime
+import sangria.ast.StringValue
 
 
 object GraphQLSchema {
 
-  implicit val LinkType = deriveObjectType[Unit, Link]()
+  implicit val GraphQLDateTime = ScalarType[DateTime](
+    "DateTime",
+    coerceOutput = (dt, _) => dt.toString,
+    coerceInput = {
+      case StringValue(dt, _, _) => DateTime.fromIsoDateTimeString(dt).toRight(DateTimeCoerceViolation)
+      case _ => Left(DateTimeCoerceViolation)
+    },
+    coerceUserInput = {
+      case s: String => DateTime.fromIsoDateTimeString(s).toRight(DateTimeCoerceViolation)
+      case _ => Left(DateTimeCoerceViolation)
+    }
+  )
+
+  // implicit val LinkType = ObjectType[Unit, Link](
+  //   "Link",
+  //   fields[Unit, Link](
+  //     Field("id", IntType, resolve = _.value.id),
+  //     Field("url", StringType, resolve = _.value.url),
+  //     Field("description", StringType, resolve = _.value.description),
+  //     Field("createdAt", GraphQLDateTime, resolve = _.value.createdAt)
+  //   )
+  // )
+  val LinkType = deriveObjectType[Unit, Link](
+    ReplaceField("createdAt", Field("createdAt", GraphQLDateTime, resolve = _.value.createdAt))
+  )
   implicit val linkHasId = HasId[Link, Int](_.id)
 
   val linksFetcher = Fetcher(
--- src/main/scala/com/howtographql/scala/sangria/models/package.scala
+++ src/main/scala/com/howtographql/scala/sangria/models/package.scala
@@ -1,7 +1,12 @@
 package com.howtographql.scala.sangria
 
 import akka.http.scaladsl.model.DateTime
+import sangria.validation.Violation
 
 package object models {
   case class Link(id: Int, url: String, description: String, createdAt: DateTime)
+
+  case object DateTimeCoerceViolation extends Violation {
+    def errorMessage: String = "Error during parsing DateTime"
+  }
 }
